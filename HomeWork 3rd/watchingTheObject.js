a3c6fca94df24e0eba2fa28ec010c35c3ea72eec
SemeniukO <32882376+SemeniukO@users.noreply.github.com>
2017-10-31 20:59:29 +0200

    Create watchingTheObject.js

---
+++ HomeWork 3rd/watchingTheObject.js
@@ -0,0 +1,28 @@
+	user = {
+		student:'Kevin',
+		surname:'Fox',
+		age:18,
+		sad:32,
+	}
+
+function check(user){
+	var surname = user.surname;
+	Object.defineProperty(user, 'surname', {
+		get: function() {
+    		console.log('The property surname  was called');
+    		return surname;
+    	},
+    	set: function(value) {
+      		surname = value;
+      		console.log('The property student  was changed');
+    	}
+  	});
+}
+
+check(user);
+
+alert(user.surname);
+
+user.surname = 1231;
+
+alert(user.surname);
