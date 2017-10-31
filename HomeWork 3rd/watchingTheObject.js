ea671770325fcf67efd32672e19a0b4822cd3e8b
SemeniukO <32882376+SemeniukO@users.noreply.github.com>
2017-10-31 21:02:20 +0200

    Update watchingTheObject.js

--- HomeWork 3rd/watchingTheObject.js
+++ HomeWork 3rd/watchingTheObject.js
@@ -1,22 +1,22 @@
-	user = {
-		student:'Kevin',
-		surname:'Fox',
-		age:18,
-		sad:32,
-	}
+user = {
+    student:'Kevin',
+    surname:'Fox',
+    age:18,
+    sad:32,
+}
 
 function check(user){
-	var surname = user.surname;
-	Object.defineProperty(user, 'surname', {
-		get: function() {
-    		console.log('The property surname  was called');
-    		return surname;
+    var surname = user.surname;
+    Object.defineProperty(user, 'surname', {
+	get: function() {
+	    console.log('The property surname  was called');
+	    return surname;
     	},
     	set: function(value) {
-      		surname = value;
-      		console.log('The property student  was changed');
+      	    surname = value;
+      	    console.log('The property student  was changed');
     	}
-  	});
+    });
 }
 
 check(user);
