842671c78ca17111f33831738b522e475c601ca2
SemeniukO <32882376+SemeniukO@users.noreply.github.com>
2017-11-02 18:09:43 +0200

    Update watchingTheObject.js

--- HomeWork 3rd/watchingTheObject.js
+++ HomeWork 3rd/watchingTheObject.js
@@ -1,30 +1,28 @@
 var user = {
-	name:'user',
-	student:'Kevin',
-	surname:'Fox',
-	age:18
-    }
-
+name:'user',
+student:'Kevin',
+surname:'Fox',
+age:18
+}
 
 function watchingTheObjec(user){
    var name = user.name || 'The object name wasn"t set';
    var obj = {};
    for (var key in user){
-	obj[key] = user[key];
+      obj[key] = user[key];
 	
-	function setTheProp(i){
-	   Object.defineProperty(user, i, {
-	      get: function() {
-	         console.log('The property (.'+i+') of object ('+name+') was called');
-    		 return obj[i];
-    	      },
-    	      set: function(value) {
-      	         obj[i] = value;
-      		 console.log('The property (.'+i+') of object ('+name+') was changed');
-    	      }
-  	   });
-	}
-     setTheProp(key);
+      function setTheProp(i){
+         Object.defineProperty(user, i, {
+            get: function() {
+               console.log('The property (.'+i+') of object ('+name+') was called');
+               return obj[i];
+    	    },
+    	    set: function(value) {
+      	       obj[i] = value;
+      	       console.log('The property (.'+i+') of object ('+name+') was changed');
+    	    }
+         });
+     }setTheProp(key);
    }
 }
 
