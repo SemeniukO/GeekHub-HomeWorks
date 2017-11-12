c8c1fd08c9935e50be7b8dba07379d43f88ecc98
SemeniukO <32882376+SemeniukO@users.noreply.github.com>
2017-11-12 20:44:00 +0200

    Create matrix.js

---
+++ HomeWork 5th/matrix.js
@@ -0,0 +1,83 @@
+var arr = [];
+var i = 0;
+while(arr.length!=20){
+	arr[i] = [];
+	var j=0;
+	while(j!=20){
+		arr[i][j] = randomInteger()
+		j++;
+	}
+	i++;
+}
+
+function randomInteger() {
+    var rand = Math.round(Math.random()*10);
+    if (rand>=1){return '.'};
+    if (rand<1){return '*'};
+  }
+
+function randomStand() {
+    var rand = Math.floor(Math.random()*20);
+    return rand;
+  }
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+var fir = randomStand();
+var sec = randomStand();
+arr[fir][sec] = '@';
+
+console.log('The animal stands here arr['+fir+']['+sec+']');
+
+function stepX(){
+	fir+= getRndInteger(-1, 1);
+	if ((fir<-1) || (fir>20)){
+		return stepX();
+	}
+	return fir;
+}
+
+function stepY(){
+	sec+= getRndInteger(-1, 1);
+	if ((sec<-1) || (sec>20)){
+		return stepX();
+	}
+	return sec;
+}
+
+function move(){
+	var lastX = fir;
+	var lastY = sec;
+	var x = stepX();
+	var y = stepY()
+	
+	if ((x==lastX)&&(y==lastY)){return move()}
+	
+	if ((x==-1)||(x==20)||(y==-1)||(y==20)){
+		console.log('This is the fence. The animal can\'t go beyond the fence');
+		fir = lastX;
+		sec = lastY;
+		return;
+	}
+	
+	if (arr[x][y] == '.'){ 
+		arr[x][y] = '@';
+		arr[lastX][lastY] = '.';
+		console.log('The animal is here right now arr['+x+']['+y+']');
+		return;
+		}
+		else { 
+			console.log('The tree is front of. The animal is thinking where to go');
+			fir = lastX;
+			sec = lastY;
+			setTimeout(function(){},3000);
+			return;
+	} 
+} 
+
+
+var timerId = setInterval(move,3000);
+
+//clearInterval(timerId);
