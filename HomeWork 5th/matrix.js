85dc966a56a8f882d86eccfd7d88eab9cd60628f
SemeniukO <32882376+SemeniukO@users.noreply.github.com>
2017-11-14 12:46:58 +0200

    Update matrix.js

--- HomeWork 5th/matrix.js
+++ HomeWork 5th/matrix.js
@@ -12,22 +12,23 @@ while(arr.length!=20){
 
 function fillArray() {
 	var rand = Math.round(Math.random()*10);
-	if (rand>=1){return '.'};
-	if (rand<1){return '*'};
+    	if (rand>=1){return '.'};
+    	if (rand<1){return '*'};
   }
 
 function putAnimal() {
-	var rand = Math.floor(Math.random()*20);
-	return rand;
+    	var rand = Math.floor(Math.random()*20);
+    	return rand;
   }
 
 function getInteger(min, max) {
-	return Math.floor(Math.random() * (max - min + 1) ) + min;
+   	 return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 
 var fir = putAnimal();
 var sec = putAnimal();
 arr[fir][sec] = '@';
+htmlWrite();
 
 console.log('The animal stands here arr['+fir+']['+sec+']');
 
@@ -57,6 +58,8 @@ function move(){
 	
 	if ((x==-1)||(x==20)||(y==-1)||(y==20)){
 		console.log('This is the fence. The animal can\'t go beyond the fence');
+		htmlWrite();
+		document.close();
 		fir = lastX;
 		sec = lastY;
 		return;
@@ -66,17 +69,36 @@ function move(){
 		arr[x][y] = '@';
 		arr[lastX][lastY] = '.';
 		console.log('The animal is here right now arr['+x+']['+y+']');
-		return;
+		htmlWrite();
+		document.close();
+        return;
 		}
 		else { 
+			clearInterval(timerId);
 			console.log('The tree is front of. The animal is thinking where to go');
+			htmlWrite();
+			document.close();
 			fir = lastX;
 			sec = lastY;
+			setTimeout(function(){timerId = setInterval(move,1000)},5000);
 			return;
 	} 
 } 
 
+var timerId = setInterval(move,1000);
 
-var timerId = setInterval(move,3000);
+function htmlWrite(){
+	var outPut = "";
+	var length = arr.length;
+	for (var i = 0; i < length; i++) {
+  		var l = arr[i].length;
+  			for (var j = 0; j < l; j++) {
+    				outPut += arr[i][j] + " ";
+  			}
+  		outPut += '</br>';
+  		document.write(outPut);
+  		outPut = '';
+	}
+}
 
-//clearInterval(timerId);
+ //clearInterval(timerId);
