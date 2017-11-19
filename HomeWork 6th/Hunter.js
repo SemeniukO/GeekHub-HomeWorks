aa0399cecb1328188aa3a31353ef76970abe2935
SemeniukO <32882376+SemeniukO@users.noreply.github.com>
2017-11-19 17:19:28 +0200

    Create Hunter.js

---
+++ HomeWork 6th/Hunter.js
@@ -0,0 +1,10 @@
+import Animal from './Animal.js';
+import {Human}  from './Human.js';
+export default class Hunter extends Human {
+	func() {
+		console.log(this._move);
+	}
+	doSomething () {
+	console.log('do something');
+	}
+}
