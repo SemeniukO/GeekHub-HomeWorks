9ba283736cbf868c1d654fa94e0b3fd1da579296
SemeniukO <32882376+SemeniukO@users.noreply.github.com>
2017-11-19 17:13:27 +0200

    Create import_test.js

---
+++ HomeWork 6th/import_test.js
@@ -0,0 +1,31 @@
+import Animal from './Animal.js';
+import {Mouse}  from './Mouse.js';
+import {Eagle} from './Eagle.js';
+import {Deer} from './Deer.js';
+import {Human} from './Human.js';
+import Hunter from './Hunter.js';
+import Aborigine from './Aborigine.js';
+
+let mouse = new Mouse();
+mouse.enable();
+mouse.func();
+
+let eagle = new Eagle();
+eagle.func();
+
+let deer = new Deer();
+deer.enable();
+deer.func();
+
+let human = new Human();
+human.enable();
+human.func();
+
+let hunter = new Hunter();
+hunter.enable();
+hunter.func();
+hunter.doSomething();
+
+let aborigine = new Aborigine();
+aborigine.func();
+aborigine.doSomething();
