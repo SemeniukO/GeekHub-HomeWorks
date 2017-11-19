6aa8cfa0e1db81f80d7d1e5e26c8c01227271f96
SemeniukO <32882376+SemeniukO@users.noreply.github.com>
2017-11-19 22:00:01 +0200

    Update import_test.js

--- HomeWork 6th/import_test.js
+++ HomeWork 6th/import_test.js
@@ -9,19 +9,7 @@ mouse.func();
 let eagle = new Eagle();
 eagle.func();
 
-let deer = new Deer();
-deer.enable();
-deer.func();
-
-let human = new Human();
-human.enable();
-human.func();
-
 let hunter = new Hunter();
 hunter.enable();
 hunter.func();
 hunter.doSomething();
-
-let aborigine = new Aborigine();
-aborigine.func();
-aborigine.doSomething();
